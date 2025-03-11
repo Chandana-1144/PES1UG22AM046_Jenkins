fceaf04ec1cb9b3123c021299eb6b35aaec4b14b
Chandana-1144 <142608532+Chandana-1144@users.noreply.github.com>
2025-03-11 16:45:10 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "Hello, PES1UG22AM046\n";
+  cout << "hello";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
